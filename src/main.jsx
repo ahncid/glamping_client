7282084d6a94c96d3be018f87e144840db60fe06
ahncid <ahncid@gmail.com>
2024-11-20 17:55:48 +0100

    browserrouter -> hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { LikeProvider } from "./context/likeContext.jsx";
 import { AuthContextProvider } from "./context/authContext";
 
@@ -9,15 +10,16 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/glamping_client">
+    <HashRouter>
       <AuthContextProvider>
         <LikeProvider>
           <App />
         </LikeProvider>
       </AuthContextProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
+
 
 // React context providers til at håndtere globale tilstande.
 // Man undgår props drilling, fordi der her opbevares og deles data som flere komponenter har brug for.
