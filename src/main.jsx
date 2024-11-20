258122e8f6056c685c3988710e8210b84f28beb2
ahncid <ahncid@gmail.com>
2024-11-20 16:59:14 +0100

    main.jsx ændring

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/glamping_client">
+    <BrowserRouter>
       <AuthContextProvider>
         <LikeProvider>
           <App />
