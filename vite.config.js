27310f99791a853888e090b94377f692424e700d
ahncid <ahncid@gmail.com>
2024-11-20 17:15:01 +0100

    vite.config og main.jsx ændring

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => ({
-    base: mode === 'development' ? '/glamping_client/' : '/',
-    plugins: [react()]
-}));
+export default defineConfig({
+  base: '/glamping_client/', 
+  plugins: [react()],
+});
